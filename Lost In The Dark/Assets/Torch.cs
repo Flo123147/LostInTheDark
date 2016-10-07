ecaae45dfb9b4ccb86f32280f7ccd3a28cabcc01
Flo123147 <f.mehlich@gmx.de>
2016-10-07 18:57:30 +0200

    161007-Update

---
+++ Lost In The Dark/Assets/Torch.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Torch : MonoBehaviour {
+
+
+	public Animator anim;
+	public bool isOn;
+
+	// Use this for initialization
+	void Awake () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetBool ("IsOn", isOn);
+	}
+}
